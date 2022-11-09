a5954006d2d7c9145957b3c996abfb585a039a28
miso <misa.0429@docomo.ne.jp>
2022-11-09 06:29:46 +0000

    Update 15 files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  console.log(div);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);
